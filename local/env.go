f02039b3d4eb42b89464e656cf1af5d6ea839782
hz <hedzrz@gmail.com>
2024-11-24 07:33:58 +0800

    update env provider

--- local/env.go
+++ local/env.go
@@ -2,6 +2,7 @@ package local
 
 import (
 	"context"
+	"strings"
 
 	logz "github.com/hedzr/logg/slog"
 	"github.com/hedzr/store"
@@ -31,6 +32,29 @@ import (
 //	); err != nil {
 //	    logz.Error("Application Error:", "err", err)
 //	}
+//
+// EnvVar-loader finds out envvars which has appname prefix (eg "TINY_APP_"),
+// and strips the prefix and parses the key-value into Store.
+//
+// For example, an app named "tiny-app", these following vars present:
+//
+//	TINY_APP_COOL_S1=1
+//	TINY_APP_COOL_S2=wow
+//
+// After cmdr loaded (before parsing), the envvars will be mapped into store as:
+//
+//	cool.               # mapped from TINY_APP_xxx
+//	  s1  -> 1
+//	  s2  -> "wow"
+//	app.cmd. ...        # another top level node, mapping from app command system
+//	app. ...            # default top level node, loading from external config files
+//
+// What difference with [cli.Config.AutoEnv]?
+//
+// AutoEnv is a builtin feature which maps APP_(subcmds)_(flag) env var value as
+// cmdr [cli.Flag]'s default value.
+//
+// For example, APP_JUMP_TO_FULL=1 will map to (root)/{Cmd:jump}/{Cmd:to}/{Flag:full} as true.
 func NewEnvVarLoader() *envvarloader {
 	return &envvarloader{}
 }
@@ -39,13 +63,18 @@ type envvarloader struct{}
 
 func (w *envvarloader) Load(ctx context.Context, app cli.App) (err error) {
 	conf := app.Store()
-	name := app.Name()
+	prefix := conf.Prefix()
+	name := strings.Replace(app.Name(), "-", "_", -1)
+	namePrefixed := name
+	if !strings.HasSuffix(namePrefixed, "_") {
+		namePrefixed = namePrefixed + "_"
+	}
 	_, err = conf.Load(ctx,
 		store.WithProvider(env.New(
-			env.WithStorePrefix(conf.Prefix()),
-			env.WithPrefix(name+"_", name+"_"),
-			env.WithLowerCase(true),
-			env.WithUnderlineToDot(true),
+			env.WithStorePrefix(prefix),                // load into store, at position `prefix`, eg. "app.envvars"
+			env.WithPrefix(namePrefixed, namePrefixed), // only these envvars if has `name`_ prefix, eg. "TINY_APP_"
+			env.WithLowerCase(true),                    // convert names to lowercase
+			env.WithUnderlineToDot(true),               // split to (dotted-) tree with delimiter '_'
 		)),
 		// store.WithStorePrefix("app.cmd"),
 	)
