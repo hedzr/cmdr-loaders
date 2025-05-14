bcca50df5653121ff2328ff38b5797741ca69acf
hz <hedzrz@gmail.com>
2025-05-14 09:08:45 +0800

    fix wrong bump and rebump to v1.3.17

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -1,9 +1,7 @@
 package main
 
-import "github.com/hedzr/cmdr/v2"
-
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "cmdr-loaders" // appName of hedzr/cmdr-loaders package
-const version = cmdr.Version   // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+const version = "v1.3.17"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
 const Version = version
