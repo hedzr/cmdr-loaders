6e635d9c0a2df5610a3a7244be56ad92858f4396
hz <hedzrz@gmail.com>
2025-06-29 12:52:12 +0800

    bump to v1.3.37

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -3,5 +3,5 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "cmdr-loaders" // appName of hedzr/cmdr-loaders package
-const version = "v1.3.36"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+const version = "v1.3.37"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
 const Version = version
