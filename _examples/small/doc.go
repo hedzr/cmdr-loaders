f047de33bc2ec893ade440e6ca8fa140714ebef5
hz <hedzrz@gmail.com>
2026-01-24 18:51:39 +0800

    bump to v1.3.67
      - upgrade deps & security patch

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -2,6 +2,8 @@ package main
 
 const AppNameExample = "small" // appName for the current demo app
 
-const appName = "cmdr-loaders" // appName of hedzr/cmdr-loaders package
-const version = "v1.3.66"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
-const Version = version
+const (
+	appName = "cmdr-loaders" // appName of hedzr/cmdr-loaders package
+	version = "v1.3.67"      // version of hedzr/cmdr-loaders package | update it while bumping hedzr/cmdr-loaders' version
+	Version = version
+)
